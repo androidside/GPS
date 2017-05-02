82e67c8c4e1fb4954c25e0fd8eafe7b10e4beb13
U-GS66-WHITE\labuser <GS66-WHITE+labuser@GS66-WHITE.ndc.nasa.gov>
2017-05-02 13:09:36 -0400

    links

--- javaSource/com/utils/GPS/GPSReadings.java
+++ javaSource/com/utils/GPS/GPSReadings.java
@@ -1,5 +1,6 @@
 package com.utils.GPS;
-
+//https://store.uputronics.com/?route=product/product&product_id=83 gps Model uBLOX MAX-M8Q SAW/LNA Breakout With Sarantel Antenna 5V 
+//http://gpsinformation.net/main/gpslock.htm
 public class GPSReadings {
 	
 		public float latitudeDegrees;
